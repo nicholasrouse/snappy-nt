8dd8b4ea633a05c89f3aab55113cd968b15771b9
Nicholas Rouse <nicholasrouse@outlook.com>
2020-08-22 00:13:57 -0500

    ManifoldAP should be able to compute trace fields now. Added gitignore file; in turn removed pycache from tracked changes. Added a few other files.

--- snappynt/ManifoldAP.py
+++ snappynt/ManifoldAP.py
@@ -1,27 +1,47 @@
 import snappy, denominatorsforsnappy
-from sage.all import factor, NumberField
+from sage.all import factor, NumberField, QuaterionAlgebra
 import math
 import functools
 
 
 class ManifoldAP(snappy.Manifold):
+    # Probably make these changeable via a class method at some point.
+    # Not sure if this a great pattern to have these as class level attributes.
+    # It's possible it should be managed on a per instance level.
     default_starting_prec = 1000
     default_starting_degree = 10
-    default_max_prec = 10 ** 5
+    default_max_prec = 10 ** 6
     default_max_degree = 100
-    default_prec_increment = 2000
+    default_prec_increment = 5000
     default_degree_increment = 5
 
-    def __init__(self):
-        snappy.Manifold.__init__(self)
+    def __init__(self, spec=None):
+        snappy.Manifold.__init__(self, spec)
         # We store the fields as a sage NumberField objects with a *monic* generating polynomial.
+        # Perhaps subclass Sage's NumberField to store all this info?
+        # The prec_record variables record whether a given prec and degree wsa sucessful.
         self.trace_field = None  
         self.trace_field_numerical_root = None 
         self.trace_field_generators = None
+        self.trace_field_prec_record = dict()
         self.invariant_trace_field = None
+        self.invariant_trace_field_numerical_root = None
         self.invariant_trace_field_generators = None
+        self.invariant_trace_field_prec_record = dict()
         self.quaternion_algebra = None
-        self.invariant_trace_field = None
+    
+    def has_two_torsion_in_homology(self):
+        """
+        Returns True if there is two-torsion in homology and False if not. This doesn't
+        really need arbitrary precision, but it hopefully makes some other code
+        cleaner.
+        """
+        homology = self.homology()
+        elementary_divisors = homology.elementary_divisors()
+        for divisor in elementary_divisors:
+            if divisor%2 == 0:
+                return True
+        return False
 
     def defining_function(self, prec):
         return snappy.snap.polished_holonomy(bits_prec=prec)
@@ -33,39 +53,43 @@ class ManifoldAP(snappy.Manifold):
         exact_field_data = approx_trace_field.find_field(
             prec=prec, degree=degree, optimize=True
         )
-        if exact_field is not None:
-            exact_field[0] = self.trace_field
-            exact_field[1] = self.trace_field_numerical_root
-            exact_field[2] = self.trace_field_generators
-        return exact_field
+        if exact_field_data is not None:
+            self.trace_field = exact_field_data[0]
+            self.trace_field_numerical_root = exact_field_data[1] 
+            self.trace_field_generators = exact_field_data[2] 
+        return self.trace_field
 
     def compute_trace_field(
         self,
-        starting_prec=ManifoldAP.default_starting_prec,
-        starting_degree=ManifoldAP.default_starting_degree,
+        starting_prec=default_starting_prec,
+        starting_degree=default_starting_degree,
         prec_increment=default_prec_increment,
         degree_increment=default_degree_increment,
-        max_prec=None,
-        max_degree=None,
+        max_prec=default_max_prec,
+        max_degree=default_max_degree,
         verbosity=False,
+        use_last_known_failed=False
     ):
         """
         This is the exact field, returned as a sage NumberField. The exact generators for the
         field are not returned by this method to allow for easier interface with sage. They are
-        however computed and stored as a class attribute for later use. The method has a
+        however computed and stored as an  attribute for later use. The method has a
         semicomplicated interface to allow for multiple attempts to find the field. If only one
         attempt is required (e.g. because the requisite precision and degree are known), the
         method compute_trace_field_fixed_prec is probably better and will store the result if
         successful.
+
+        It's possible I should code a decorator for the variable precision pattern.
          
         Aug-1-2020
         """
         if self.trace_field:
             return self.trace_field
+        elif self.invariant_trace_field and not self.has_two_torsion_in_homology():
+            return self.invariant_trace_field
         else:
             prec = starting_prec
             degree = starting_degree
-            approx_trace_field = snappy.snap.trace(self)
             exact_field = None
             while exact_field == None:
                 if verbosity:
@@ -73,9 +97,10 @@ class ManifoldAP(snappy.Manifold):
                         str(manifold) + ":",
                         f"Trying with precision={prec} and degree={degree}",
                     )
-                exact_field = compute_trace_field_fixed_prec(
-                    manifold, prec=prec, degree=degree
+                exact_field = ManifoldAP.compute_trace_field_fixed_prec(
+                    self, prec=prec, degree=degree
                 )
+                self.trace_field_prec_record[(prec, degree)] = bool(exact_field)
                 if prec == max_prec and degree == max_degree:
                     return None
                 if prec + prec_increment <= max_prec:
@@ -87,3 +112,8 @@ class ManifoldAP(snappy.Manifold):
                 else:
                     degree = max_degree
             return self.trace_field
+    
+    def compute_quaternion_algebra_fixed_prec(self, prec=default_starting_prec, degree=default_starting_degree):
+        if not self.trace_field:
+            self.compute_trace_field_fixed_prec(prec=prec, degree=degree)
+        
---
+++ snappynt/misc_functions.py
@@ -0,0 +1,3 @@
+"""
+Module for odds and ends functions that thematically didn't fit into another module.
+"""

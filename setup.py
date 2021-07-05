f3b26aef6cc17e41d83578e0c623112e5e8444a2
Nicholas Rouse <nicholasrouse@outlook.com>
2021-07-05 18:44:55 -0500

    Change directory structure (again) to src-style one.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from glob import glob
+from os.path import basename, splitext
+
+from setuptools import find_packages, setup
+
+setup(
+    name="snappy-nt",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
+)

f0c1e24bb54cb85d0c1a418849188ea9c9a7927c
Nicholas Rouse <nicholasrouse@outlook.com>
2021-07-09 17:08:21 -0500

    Add versioning and snappy dependency.

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ from setuptools import find_packages, setup
 
 setup(
     name="snappynt",
+    version="0.2.0",
+    install_requires=["snappy==3.0.1"],
     packages=find_packages("src"),
     package_dir={"": "src"},
     py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],

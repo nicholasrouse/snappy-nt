e52a763c433966bb6a0d3975cb43cf37fc660943
Nicholas Rouse <nicholasrouse@outlook.com>
2021-07-20 04:13:28 -0500

    Add more tests for ManifoldNT.

--- tests/ManifoldNT_test.py
+++ tests/ManifoldNT_test.py
@@ -1,4 +1,5 @@
 import pytest
+from sage.all import NumberField, var
 
 import snappynt.ManifoldNT as ManifoldNT
 
@@ -17,6 +18,34 @@ def m010minusOneTwo_computed():
     while not mfld._arithmetic_invariants_known():
         mfld.compute_arithmetic_invariants()
     return mfld
+
+
+@pytest.fixture
+def nine_fourteen_trace_field():
+    x = var("x")
+    poly = (
+        x ** 18
+        - 3 * x ** 17
+        - 5 * x ** 16
+        - 4 * x ** 15
+        + 192 * x ** 14
+        - 2375 * x ** 13
+        + 14845 * x ** 12
+        - 36312 * x ** 11
+        + 36785 * x ** 10
+        - 213737 * x ** 9
+        + 1454725 * x ** 8
+        - 4869252 * x ** 7
+        + 10456460 * x ** 6
+        - 16339634 * x ** 5
+        + 19631702 * x ** 4
+        - 21382208 * x ** 3
+        + 24861005 * x ** 2
+        - 22425163 * x
+        + 9093699
+    )
+    field = NumberField(poly, "z")
+    return field
 
 
 def test_fix_names_error():
@@ -64,7 +93,38 @@ def test_nine_fourteen_computation():
     mfld.compute_arithmetic_invariants(prec=100, degree=5)
     while not mfld._arithmetic_invariants_known():
         mfld.compute_arithmetic_invariants()
+    mfld.compute_arithmetic_invariants()
     assert mfld._arithmetic_invariants_known()
+
+
+def test_nine_fourteen_itf_first(nine_fourteen_trace_field):
+    # We tell it the invariant trace field to check the logic for knowing the this, but
+    # not the trace field itself.
+    mfld = ManifoldNT.ManifoldNT("9_14")
+    mfld._invariant_trace_field = nine_fourteen_trace_field
+    mfld.trace_field(prec=100, degree=5)
+    while mfld._trace_field is None:
+        mfld.trace_field()
+    assert mfld._trace_field.is_isomorphic(mfld._invariant_trace_field)
+
+
+def test_nine_fourteen_itf_first2():
+    mfld = ManifoldNT.ManifoldNT("9_14")
+    while mfld._invariant_trace_field is None:
+        mfld.invariant_trace_field()
+    mfld.trace_field(prec=100, degree=5)
+    while mfld._trace_field is None:
+        mfld.trace_field()
+    assert mfld._trace_field.is_isomorphic(mfld._invariant_trace_field)
+
+
+def test_nine_fourteen_tf_first(nine_fourteen_trace_field):
+    mfld = ManifoldNT.ManifoldNT("9_14")
+    mfld._trace_field = nine_fourteen_trace_field
+    mfld.invariant_trace_field(prec=100, degree=5)
+    while mfld._invariant_trace_field is None:
+        mfld.invariant_trace_field()
+    assert mfld._trace_field.is_isomorphic(mfld._invariant_trace_field)
 
 
 def test_m141twoThree_computation():
@@ -72,6 +132,17 @@ def test_m141twoThree_computation():
     while not mfld._arithmetic_invariants_known():
         mfld.compute_arithmetic_invariants()
     assert mfld._arithmetic_invariants_known()
+
+
+def test_m141twoThree_qa_small_prec():
+    mfld = ManifoldNT.ManifoldNT("m141(2,3)")
+    while not mfld._trace_field:
+        mfld.trace_field()
+    mfld.quaternion_algebra(prec=100)
+    while not mfld._quaternion_algebra:
+        mfld.quaternion_algebra()
+    mfld.quaternion_algebra()
+    assert mfld._quaternion_algebra is not None
 
 
 def test_m010minusOneTwo_computation():

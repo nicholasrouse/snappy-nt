7503449191a80d5d81267aab78c218eb6038a73d
Nicholas Rouse <nicholasrouse@outlook.com>
2021-07-19 21:19:55 -0500

    Add more tests to ManifoldNT_test.py

--- tests/ManifoldNT_test.py
+++ tests/ManifoldNT_test.py
@@ -24,7 +24,20 @@ def test_fix_names_error():
         ManifoldNT.fix_names("bad name")
 
 
+def test_fig_eight_homology(fig_eight_computed):
+    mfld = fig_eight_computed
+    assert mfld.homology_two_rank() == 0
+
+
 def test_m010minusOneTwo_homology(m010minusOneTwo_computed):
     # Its trace field and invariant trace field are different.
     mfld = m010minusOneTwo_computed
     assert mfld.homology_two_rank() == 1
+
+
+def test_str_magic_method(fig_eight_computed):
+    assert fig_eight_computed.__str__() == "4_1(0,0)"
+
+
+def test_repr_magic_method(fig_eight_computed):
+    assert fig_eight_computed.__repr__() == "4_1(0,0)"
